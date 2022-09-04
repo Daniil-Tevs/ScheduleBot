33e04580232d383b2ade5ea423f346c69529bb0e
Daniil-Tevs <daniil_tevs@mail.ru>
2022-09-05 01:37:41 +0200

    Refactor all

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def get_text(message):
     elif message.text == "Завтра":
         bot.send_message(message.chat.id,choose_day(datetime.datetime.today().weekday()+1), parse_mode="HTML")
     else:
-        bot.send_message(message.chat.id,"Некорректная команда. Используйте /help" + str(datetime.datetime.today().weekday()))
+        bot.send_message(message.chat.id,"Некорректная команда. Используйте /help" + str(datetime.datetime.today().hour))
 
 
 if __name__ == '__main__':
