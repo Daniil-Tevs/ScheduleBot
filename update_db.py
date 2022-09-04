e934f5beaf08b4a939fae8ee584f6c270219e837
Daniil-Tevs <daniil_tevs@mail.ru>
2022-09-05 01:04:58 +0200

    Refactor all

---
+++ update_db.py
@@ -0,0 +1,24 @@
+def make_user(id, id_group, con):
+    try:
+        cur = con.cursor()
+        cur.execute(
+            "INSERT INTO user_data(user_id, id_group) VALUES ('{}', '{}')".format(str(id), str(id_group))
+        )
+
+        con.commit()
+
+    except Exception:
+        con.commit()
+
+
+def get_users_id(con):
+    try:
+        cur = con.cursor()
+        cur.execute('SELECT user_id FROM user_data')
+
+        users_id = list([i[0] for i in cur.fetchall()])
+        con.commit()
+        return users_id
+
+    except Exception:
+        con.commit()
