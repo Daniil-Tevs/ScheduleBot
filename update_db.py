0c2ed3a742705a17fc828a7843372b6868ac6466
Daniil-Tevs <daniil_tevs@mail.ru>
2022-09-05 13:53:49 +0200

    Refactor db

--- update_db.py
+++ update_db.py
@@ -1,7 +1,7 @@
 def make_user(id, id_group, cur):
     try:
         cur.execute(
-            "INSERT INTO user_data(user_id, id_group) VALUES ('{}', '{}')".format(str(id), str(id_group))
+            "INSERT INTO public.user_data(user_id, id_group) VALUES ('{}', '{}')".format(str(id), str(id_group))
         )
     except Exception:
         pass
@@ -9,7 +9,7 @@ def make_user(id, id_group, cur):
 
 def get_users_id(cur):
     try:
-        cur.execute("SELECT user_id FROM user_data")
+        cur.execute("SELECT user_id FROM public.user_data")
 
         users_id = list([i[0] for i in cur.fetchall()])
         return users_id
