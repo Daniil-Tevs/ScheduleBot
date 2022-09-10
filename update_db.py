4d739a40b50da7610163bf1ee5da6b952e9d05ec
Daniil-Tevs <daniil_tevs@mail.ru>
2022-09-10 02:26:37 +0200

    refactor connection

--- update_db.py
+++ update_db.py
@@ -12,61 +12,52 @@ con = psycopg2.connect(
 def make_user(id, id_group):
     global con
     try:
-        cur = con.cursor()
-        cur.execute(
-            "INSERT INTO user_data(user_id, id_group) VALUES ('{}', '{}')".format(str(id), str(id_group))
-        )
-        con.commit()
-        con.close()
+        with con.cursor() as cur:
+            cur.execute(
+                "INSERT INTO user_data(user_id, id_group) VALUES ('{}', '{}')".format(str(id), str(id_group))
+            )
+            con.commit()
     except Exception:
-        con.commit()
-        con.close()
+        pass
 
 
 def get_users_id():
     global con
     try:
+        with con.cursor() as cur:
+            cur.execute('SELECT user_id FROM user_data')
+            con.commit()
+            con.close()
 
-        cur = con.cursor()
-        cur.execute('SELECT user_id FROM user_data')
-        con.commit()
-        con.close()
-
-        users_id = list([i[0] for i in cur.fetchall()])
-        return users_id
+            users_id = list([i[0] for i in cur.fetchall()])
+            return users_id
 
     except Exception:
-        con.commit()
-        con.close()
+        pass
 
 
 def get_group(user_id):
     global con
     try:
-        cur = con.cursor()
-        cur.execute("SELECT id_group FROM user_data WHERE user_id='{}'".format(str(user_id)))
-        tmp = cur.fetchall()
-        if tmp:
-            group = tmp[0][0]
-            con.commit()
-            con.close()
-            return group
-        else:
-            con.commit()
-            con.close()
-            return " "
+        with con.cursor() as cur:
+            cur.execute("SELECT id_group FROM user_data WHERE user_id='{}'".format(str(user_id)))
+            tmp = cur.fetchall()
+            if tmp:
+                group = tmp[0][0]
+                con.commit()
+                return group
+            else:
+                con.commit()
+                return " "
     except Exception:
-        con.commit()
-        con.close()
+        pass
 
 
 def delete_user(user_id):
     global con
     try:
-        cur = con.cursor()
-        cur.execute("DELETE FROM user_data WHERE user_id='{}'".format(str(user_id)))
-        con.commit()
-        con.close()
+        with con.cursor() as cur:
+            cur.execute("DELETE FROM user_data WHERE user_id='{}'".format(str(user_id)))
+            con.commit()
     except Exception:
-        con.commit()
-        con.close()
+        pass
